2f80440df098321824ee0a8eadb6d1d5ccaaa082
Ryo HIGASHIGAWA <higashigawa@platinum-egg.com>
2015-09-23 01:50:56 +0900

    初期整備

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,26 @@
-var gulp = require('gulp')
-var plumber = require('gulp-plumber')
-var sass = require('gulp-sass')
-var watch = require('gulp-watch')
+var gulp = require('gulp');
+var plumber = require('gulp-plumber');
+var sass = require('gulp-sass');
+var sym = require('gulp-sym');
+var watch = require('gulp-watch');
 
 gulp.task (
     'compile',
     [
+        'compile-js',
         'compile-html',
         'compile-scss',
+        'compile-symlink'
     ]
+);
+
+gulp.task(
+    'compile-js',
+    function () {
+        return gulp.src('src/**/*.js')
+            .pipe(plumber())
+            .pipe(gulp.dest('app/'));
+    }
 );
 
 gulp.task(
@@ -17,7 +29,7 @@ gulp.task(
         gulp.src('src/**/*.html')
             .pipe(gulp.dest('app'));
     }
-)
+);
 
 gulp.task(
     'compile-scss',
@@ -26,11 +38,19 @@ gulp.task(
             .pipe(sass({ includePaths: ['node_modules'] }).on('error', sass.logError))
             .pipe(gulp.dest('app'))
     }
-)
+);
+
+gulp.task(
+    'compile-symlink',
+    function () {
+        gulp.src('node_modules/font-awesome/fonts')
+            .pipe(sym('app/renderer/fonts', { force: true }));
+    }
+);
 
 gulp.task(
     'watch',
     function () {
         gulp.watch('src/**/*', ['compile']);
     }
-)
+);
